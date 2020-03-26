2fad74071b1cb1b22eba00efdcca8bacdcc66057
Jack Airns <jackairns2009@hotmail.co.uk>
2020-03-26 16:18:10 +0000

    Version 2

--- app/src/main/java/com/example/ctrack_groupwork/Calories.java
+++ app/src/main/java/com/example/ctrack_groupwork/Calories.java
@@ -25,8 +25,8 @@ public class Calories extends AppCompatActivity {
     private TextView textView;
     private TextView textView2;
     private ProgressBar progressBar;
-    int currentCal;
-    int dailyCal;
+    int currentCal = 0;
+    int dailyCal = 0;
 
     public static final String PREFS_NAME = "MyPrefsFile";
 
@@ -87,13 +87,12 @@ public class Calories extends AppCompatActivity {
             public void onClick(DialogInterface dialogInterface, int i) {
                 // Assign value to integer
                 dailyCal = Integer.valueOf(setDailyCalInput.getText().toString());
-                // While user input equals 0, display toast
-                while (dailyCal == 0) {
+                // While user input equals 0, display toast and update shared preferences
+                while(dailyCal == 0) {
                     Toast.makeText(Calories.this, "Cannot set calories to 0", Toast.LENGTH_LONG).show();
                     break;
                 }
-                // While user input equals 0, display toast and update shared preferences
-                while (dailyCal != 0) {
+                while(dailyCal != 0) {
                     updateProgress();
                     Toast.makeText(Calories.this, "Calories set", Toast.LENGTH_LONG).show();
                     SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
@@ -102,7 +101,7 @@ public class Calories extends AppCompatActivity {
                     editor.apply();
                     break;
                 }
-            }
+             }
         });
         // If cancel is clicked, close alert dialog
         setDailyCalDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -127,12 +126,13 @@ public class Calories extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 currentCal += Integer.parseInt(addCalInput.getText().toString());
-                updateProgress();
-                Toast.makeText(Calories.this, "Calories added", Toast.LENGTH_LONG).show();
-                @SuppressLint("WrongConstant") SharedPreferences settings2 = getSharedPreferences(PREFS_NAME, 1);
-                SharedPreferences.Editor editor = settings2.edit();
-                editor.putInt("currentCal", currentCal);
-                editor.apply();
+                    updateProgress();
+                    Toast.makeText(Calories.this, "Calories added", Toast.LENGTH_LONG).show();
+                    @SuppressLint("WrongConstant") SharedPreferences settings2 = getSharedPreferences(PREFS_NAME, 1);
+                    SharedPreferences.Editor editor = settings2.edit();
+                    editor.putInt("currentCal", currentCal);
+                    editor.apply();
+
             }
         });
         // If cancel is clicked, close alert dialog
@@ -150,34 +150,41 @@ public class Calories extends AppCompatActivity {
         // Convert percentage into string for display
         textView.setText(String.valueOf(currentCal));
         textView2.setText(String.valueOf(dailyCal));
-        // Convert into float for calculation
-        float percentage = (currentCal * 100/ dailyCal);
-        // Convert percentage into integer for progress bar
-        int progInt = (int)percentage;
-        // Set progress bar
-        progressBar.setProgress(progInt);
-        // if percentage is greater than 99% update text colour to red - notifying the user they have reached their daily limit
-        if(percentage > 99) {
-            textView.setTextColor(Color.parseColor("#fe4a49"));
-            progressBar.getProgressDrawable().setColorFilter(
-                    Color.parseColor("#fe4a49"), android.graphics.PorterDuff.Mode.SRC_IN);
-        }
-        // if percentage is between 70 and 99 percent update text colour
-        else if (percentage > 70 && percentage <= 99) {
-            textView.setTextColor(Color.parseColor("#fed766"));
-            progressBar.getProgressDrawable().setColorFilter(
-                    Color.parseColor("#fed766"), android.graphics.PorterDuff.Mode.SRC_IN);
-        }
-        // if percentage is below 70 set text text colour
-        else {
-            textView.setTextColor(Color.parseColor("#2ab7ca"));
-            progressBar.getProgressDrawable().setColorFilter(
-                    Color.parseColor("#2ab7ca"), android.graphics.PorterDuff.Mode.SRC_IN);
+        // Checks value as cannot be implemented
+        if(currentCal!= 0 & dailyCal !=0) {
+            // Convert into float for calculation
+            float percentage = (currentCal * 100/ dailyCal);
+            // Convert percentage into integer for progress bar
+            int progInt = (int)percentage;
+            // Set progress bar
+            progressBar.setProgress(progInt);
+            // if percentage is greater than 99% update text colour to red - notifying the user they have reached their daily limit
+            if(percentage > 99) {
+                textView.setTextColor(Color.parseColor("#fe4a49"));
+                progressBar.getProgressDrawable().setColorFilter(
+                        Color.parseColor("#fe4a49"), android.graphics.PorterDuff.Mode.SRC_IN);
+            }
+            // if percentage is between 70 and 99 percent update text colour
+            else if (percentage > 70 && percentage <= 99) {
+                textView.setTextColor(Color.parseColor("#fed766"));
+                progressBar.getProgressDrawable().setColorFilter(
+                        Color.parseColor("#fed766"), android.graphics.PorterDuff.Mode.SRC_IN);
+            }
+            // if percentage is below 70 set text text colour
+            else {
+                textView.setTextColor(Color.parseColor("#2ab7ca"));
+                progressBar.getProgressDrawable().setColorFilter(
+                        Color.parseColor("#2ab7ca"), android.graphics.PorterDuff.Mode.SRC_IN);
+            }
         }
     }
     // Reset currentCal variable to 0, and update this in memory
     public void resetCal(View view){
         currentCal = 0;
+        progressBar.setProgress(0);
+        textView.setTextColor(Color.parseColor("#2ab7ca"));
+        progressBar.getProgressDrawable().setColorFilter(
+                Color.parseColor("#2ab7ca"), android.graphics.PorterDuff.Mode.SRC_IN);
         @SuppressLint("WrongConstant") SharedPreferences settings2 = getSharedPreferences(PREFS_NAME, 1);
         SharedPreferences.Editor editor = settings2.edit();
         editor.putInt("currentCal", currentCal);
